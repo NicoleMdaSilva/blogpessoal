7b796aa862c7168216258a637f4b11fdb37c0659
PauloCMoreira <pauloskx9004@gmail.com>
2022-10-31 11:36:17 -0300

    add: constants

---
+++ src/auth/constants/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: '3995483026970396548u5u988332737326746eyye6we635255535180321' //Aqui que botaremos o 3 hashes, a chave secreta. Quem vai garantir que criaremos o token.
+};
